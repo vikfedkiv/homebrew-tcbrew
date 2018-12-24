0c410fdb0e03e2637323b45d92b3c307a8ca09c7
Viktor Fedkiv <v.fedkiv@tenantcloud.com>
2018-12-24 11:02:51 +0200

    sequel2

--- sequel.rb
+++ sequel.rb
@@ -2,4 +2,4 @@ class Sequel < Formula
    desc "Sequel"
    homepage "http://hasseg.org/trash/"
    url "http://10.10.4.242/sequel-pro-1.1.2.dmg"
-   sha256 '7b34fd63c13e9e9ca4f87d548241ff9df9a266b554af23549efd7be006f387c6'
+   sha256 "7b34fd63c13e9e9ca4f87d548241ff9df9a266b554af23549efd7be006f387c6"
