7467eb8f527268d8270e0cd450931c3d611314a9
Viktor Fedkiv <v.fedkiv@tenantcloud.com>
2018-12-24 14:12:24 +0200

    sequel18

--- nginx.rb
+++ nginx.rb
@@ -4,7 +4,7 @@ class Nginx < Formula
   # Use "mainline" releases only (odd minor version number), not "stable"
   # See https://www.nginx.com/blog/nginx-1-12-1-13-released/ for why
   url "http://10.10.4.242/nginx-1.15.7.tar.gz"
-  sha256 "..."
+  sha256 "8f22ea2f6c0e0a221b6ddc02b6428a3ff708e2ad55f9361102b1c9f4142bdf93"
   head "https://hg.nginx.org/nginx/", :using => :hg
 
   option "with-passenger", "Compile with support for Phusion Passenger module"
