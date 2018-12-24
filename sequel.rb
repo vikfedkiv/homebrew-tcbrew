f0c494e7215c91eb2c84aec255996fb0f92885ee
Viktor Fedkiv <v.fedkiv@tenantcloud.com>
2018-12-24 12:24:31 +0200

    sequel11

--- sequel.rb
+++ sequel.rb
@@ -1,13 +1,16 @@
-class Sequel < Formula
-  desc ""
-  homepage ""
+class Sequel < Cask
 
   version "1.1.2"
   sha256 "88d4baeee461da524212ac703fff90d510eb8c9cb07b42ba5bf1ab1f54eaa8ba"
-  url "http://10.10.4.242/sequel-pro-release-1.1.2.tar.gz"
+  url "http://10.10.4.242/sequel-pro-1.1.2.dmg"
 
-  def install
-    system "make", "install"
-  end
+  app "Sequel Pro.app"
+
+  zap trash: [
+               "~/Library/Application Support/Sequel Pro",
+               "~/Library/Caches/com.sequelpro.SequelPro",
+               "~/Library/Preferences/com.sequelpro.SequelPro.plist",
+               "~/Library/Saved Application State/com.sequelpro.SequelPro.savedState",
+	     ]
 
 end
