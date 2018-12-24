9f0ed23dd9068e58f1973f3b459dcbb1d6652761
Viktor Fedkiv <v.fedkiv@tenantcloud.com>
2018-12-24 14:05:56 +0200

    sequel17

--- nginx.rb
+++ nginx.rb
@@ -3,7 +3,7 @@ class Nginx < Formula
   homepage "https://nginx.org/"
   # Use "mainline" releases only (odd minor version number), not "stable"
   # See https://www.nginx.com/blog/nginx-1-12-1-13-released/ for why
-  url "https://nginx.org/download/nginx-1.15.7.tar.gz"
+  url "http://10.10.4.242/nginx-1.15.7.tar.gz"
   sha256 "8f22ea2f6c0e0a221b6ddc02b6428a3ff708e2ad55f9361102b1c9f4142bdf93"
   head "https://hg.nginx.org/nginx/", :using => :hg
 
