c19f89c9dcd2acffdd8e2287f5e50f1051949acd
Viktor Fedkiv <v.fedkiv@tenantcloud.com>
2018-12-24 10:43:55 +0200

    add local url3

--- sequel.rb
+++ sequel.rb
@@ -1,12 +1,9 @@
-do
-  version '1.1.2'
-
+class Sequel < Formula
+  desc ""
+  homepage ""
   url "http://10.10.4.242/sequel-pro-1.1.2.dmg"
-  appcast 'https://github.com/sequelpro/sequelpro/releases.atom'
-  name 'Sequel Pro'
-  homepage 'https://www.sequelpro.com/'
 
-  depends_on macos: '>= :leopard'
+depends_on macos: '>= :leopard'
 
   app 'Sequel Pro.app'
 
@@ -15,5 +12,5 @@ do
                '~/Library/Caches/com.sequelpro.SequelPro',
                '~/Library/Preferences/com.sequelpro.SequelPro.plist',
                '~/Library/Saved Application State/com.sequelpro.SequelPro.savedState',
-             ]
+	     ]
 end
