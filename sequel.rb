8b450f02d91a619eea488179633c54ad6f0ccb0b
Viktor Fedkiv <v.fedkiv@tenantcloud.com>
2018-12-24 11:01:40 +0200

    sequel1

--- sequel.rb
+++ sequel.rb
@@ -1,14 +1,5 @@
 class Sequel < Formula
-  desc ""
-  homepage ""
-  url "http://10.10.4.242/sequel-pro-1.1.2.dmg"
-
-  app 'Sequel Pro.app'
-
-  zap trash: [
-               '~/Library/Application Support/Sequel Pro',
-               '~/Library/Caches/com.sequelpro.SequelPro',
-               '~/Library/Preferences/com.sequelpro.SequelPro.plist',
-               '~/Library/Saved Application State/com.sequelpro.SequelPro.savedState',
-	     ]
-end
+   desc "Sequel"
+   homepage "http://hasseg.org/trash/"
+   url "http://10.10.4.242/sequel-pro-1.1.2.dmg"
+   sha256 '7b34fd63c13e9e9ca4f87d548241ff9df9a266b554af23549efd7be006f387c6'
