372530df072ec416edb0e8d1abd9962fc84b72f1
Viktor Fedkiv <v.fedkiv@tenantcloud.com>
2018-12-24 12:50:04 +0200

    sequel13

--- sequelpro.rb
+++ sequelpro.rb
@@ -1,12 +1,10 @@
 class Sequelpro < Formula
   desc "..."
   homepage "..."
-  url "http://10.10.4.242/sequel-pro-release-1.1.2.tar.gz"
+  url "http://10.10.4.242/sequel-pro-1.1.2.dmg"
 #  sha256 "88d4baeee461da524212ac703fff90d510eb8c9cb07b42ba5bf1ab1f54eaa8ba"
   sha256 "..."
 
-  def install
-    system "make", "install"
-  end
+  app "Sequel Pro.app"
 
 end
