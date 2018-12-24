82db71f25620c1631de6db43a21fc6fef6fe0d74
Viktor Fedkiv <v.fedkiv@tenantcloud.com>
2018-12-24 12:29:52 +0200

    sequel11

--- sequel.rb
+++ sequel.rb
@@ -1,16 +1,21 @@
-class Sequel < Cask
+cask 'sequel-pro' do
+  version '1.1.2'
+  sha256 '7b34fd63c13e9e9ca4f87d548241ff9df9a266b554af23549efd7be006f387c6'
 
-  version "1.1.2"
-  sha256 "88d4baeee461da524212ac703fff90d510eb8c9cb07b42ba5bf1ab1f54eaa8ba"
-  url "http://10.10.4.242/sequel-pro-1.1.2.dmg"
+  # github.com/sequelpro/sequelpro was verified as official when first introduced to the cask
+  url "https://github.com/sequelpro/sequelpro/releases/download/release-#{version}/sequel-pro-#{version}.dmg"
+  appcast 'https://github.com/sequelpro/sequelpro/releases.atom'
+  name 'Sequel Pro'
+  homepage 'https://www.sequelpro.com/'
 
-  app "Sequel Pro.app"
+  depends_on macos: '>= :leopard'
+
+  app 'Sequel Pro.app'
 
   zap trash: [
-               "~/Library/Application Support/Sequel Pro",
-               "~/Library/Caches/com.sequelpro.SequelPro",
-               "~/Library/Preferences/com.sequelpro.SequelPro.plist",
-               "~/Library/Saved Application State/com.sequelpro.SequelPro.savedState",
-	     ]
-
+               '~/Library/Application Support/Sequel Pro',
+               '~/Library/Caches/com.sequelpro.SequelPro',
+               '~/Library/Preferences/com.sequelpro.SequelPro.plist',
+               '~/Library/Saved Application State/com.sequelpro.SequelPro.savedState',
+             ]
 end
