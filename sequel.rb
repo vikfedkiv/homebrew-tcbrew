4718cbb78b953ae87e225f97b73b4b7088b73efe
Viktor Fedkiv <v.fedkiv@tenantcloud.com>
2018-12-24 11:15:32 +0200

    sequel3

--- sequel.rb
+++ sequel.rb
@@ -1,5 +1,9 @@
 class Sequel < Formula
-   desc "Sequel"
-   homepage "http://hasseg.org/trash/"
-   url "http://10.10.4.242/sequel-pro-1.1.2.dmg"
-   sha256 "7b34fd63c13e9e9ca4f87d548241ff9df9a266b554af23549efd7be006f387c6"
+  desc ""
+  homepage ""
+  url "http://10.10.4.242/sequel-pro-release-1.1.2.tar.gz"
+
+  def install
+  end
+
+end
