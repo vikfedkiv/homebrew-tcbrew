22bdb1edd9e5107d1d19a53ecafb06ac02b5c8e7
Viktor Fedkiv <v.fedkiv@tenantcloud.com>
2018-12-24 12:14:55 +0200

    sequel10

--- sequelpro.rb
+++ sequelpro.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Sequelpro < Formula
   desc "MySQL/MariaDB database management for macOS"
   homepage "https://sequelpro.com/"
@@ -9,26 +6,8 @@ class Sequelpro < Formula
   # depends_on "cmake" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
+    system "./configure", "--prefix=#{prefix}"
     system "make", "install" # if this fails, try separate make/make install steps
   end
 
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test sequelpro`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
-  end
 end
