5baf9fcb4dfd0fd8824eded0e25e39087db7f780
Viktor Fedkiv <v.fedkiv@tenantcloud.com>
2018-12-24 12:55:10 +0200

    sequel15

--- sequelpro.rb
+++ sequelpro.rb
@@ -1,10 +1,12 @@
-Sequelpro < Formula do
+class Sequelpro < Formula
   desc "..."
   homepage "..."
   url "http://10.10.4.242/sequel-pro-1.1.2.dmg"
 #  sha256 "88d4baeee461da524212ac703fff90d510eb8c9cb07b42ba5bf1ab1f54eaa8ba"
   sha256 "..."
-
+  
+  do
   app "Sequel Pro.app"
+  end
 
 end
