7f32daf377c2459781cb4cfd842e8d282d2fd4c8
Viktor Fedkiv <v.fedkiv@tenantcloud.com>
2018-12-24 11:30:33 +0200

    sequel6

--- sequel.rb
+++ sequel.rb
@@ -1,7 +1,7 @@
-vikfedkiv/tcbrew 'sequel' do
+vikfedkiv 'sequel' do
   version '1.1.2'
-  sha256 "88d4baeee461da524212ac703fff90d510eb8c9cb07b42ba5bf1ab1f54eaa8ba"
-  url "http://10.10.4.242/sequel-pro-release-1.1.2.tar.gz"
+  sha256 '88d4baeee461da524212ac703fff90d510eb8c9cb07b42ba5bf1ab1f54eaa8ba'
+  url 'http://10.10.4.242/sequel-pro-release-1.1.2.tar.gz'
 
   name 'Seqeul Pro'
   homepage 'https://flyingmeat.com/acorn/'
