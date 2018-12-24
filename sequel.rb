6bf82be804839d7656c3a9595306d39ef9df293b
Viktor Fedkiv <v.fedkiv@tenantcloud.com>
2018-12-24 11:27:28 +0200

    sequel5

--- sequel.rb
+++ sequel.rb
@@ -1,10 +1,18 @@
-class Sequel < Formula
-  desc ""
-  homepage ""
-  url "http://10.10.4.242/sequel-pro-release-1.1.2.tar.gz"
+vikfedkiv/tcbrew 'sequel' do
+  version '1.1.2'
   sha256 "88d4baeee461da524212ac703fff90d510eb8c9cb07b42ba5bf1ab1f54eaa8ba"
+  url "http://10.10.4.242/sequel-pro-release-1.1.2.tar.gz"
 
-  def install
-  end
+  name 'Seqeul Pro'
+  homepage 'https://flyingmeat.com/acorn/'
+
+  app 'Sequel Pro.app'
+
+  zap trash: [
+               '~/Library/Application Support/Sequel Pro',
+               '~/Library/Caches/com.sequelpro.SequelPro',
+               '~/Library/Preferences/com.sequelpro.SequelPro.plist',
+               '~/Library/Saved Application State/com.sequelpro.SequelPro.savedState',
+	     ]
 
 end
