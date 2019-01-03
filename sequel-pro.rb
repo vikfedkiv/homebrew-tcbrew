82eca9535792005644efcc30090139fb755de23e
Viktor Fedkiv <v.fedkiv@tenantcloud.com>
2019-01-03 10:23:47 +0200

    test sequel2

--- sequel-pro.rb
+++ sequel-pro.rb
@@ -1,10 +1,10 @@
-class Sequel Pro < Formula
+class sequelpro < Formula
   desc "Sequel Pro"
   homepage "http://sequelpro.com"
   url "http://10.10.0.134/release-1.1.2.tar.gz"
   sha256 "88d4baeee461da524212ac703fff90d510eb8c9cb07b42ba5bf1ab1f54eaa8ba"
 
   def install
-        bin.install "sequel-pro"
+        bin.install "sequelpro"
   end
 end
