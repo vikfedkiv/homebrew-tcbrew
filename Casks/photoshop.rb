e61fe21ad302016d6379acba022cf0580d7a75a1
Viktor Fedkiv <v.fedkiv@tenantcloud.com>
2019-01-04 09:25:30 +0200

    photoshop test2

--- Casks/photoshop.rb
+++ Casks/photoshop.rb
@@ -1,7 +1,7 @@
 class Photoshop < Cask
   url "http://10.10.4.242/Adobe%20Photoshop.dmg"
   homepage 'https://testtc.com'
-  version '201901'
-  sha256 '3cb3be0b2e5278d4eb27b3265a4a67044a2c1e11bc902fc15a6a237833dcc6e8'
-  link 'Adobe Photoshop.app'
+  version '201901'  
+  no_checksum
+  link 'Photoshop.app'
 end
