962b4d9badb6d000ce0ebc1a0d78f2b53f79807e
Viktor Fedkiv <v.fedkiv@tenantcloud.com>
2019-01-04 09:29:28 +0200

    add 256sum

--- photoshop.rb
+++ photoshop.rb
@@ -1,7 +1,7 @@
 class Photoshop < Formula
   url "http://10.10.4.242/Adobe%20Photoshop.dmg"
+  sha256 "3cb3be0b2e5278d4eb27b3265a4a67044a2c1e11bc902fc15a6a237833dcc6e8"
   homepage 'https://testtc.com'
   version '201901'  
-  no_checksum
   link 'Photoshop.app'
 end
