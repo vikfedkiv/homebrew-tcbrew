64203bb3de9bea5a81d1bcf357117f69718e8581
Viktor Fedkiv <v.fedkiv@tenantcloud.com>
2019-01-04 08:59:15 +0200

    photostop rb

--- Casks/photoshop.rb
+++ Casks/photoshop.rb
@@ -1,15 +1,7 @@
-cask 'photoshop' do
-  version '2019'
-  sha256 '3cb3be0b2e5278d4eb27b3265a4a67044a2c1e11bc902fc15a6a237833dcc6e8'
-
-  # github.com/sequelpro/sequelpro was verified as official when first introduced to the cask
+class Photoshop < Cask
   url "http://10.10.4.242/Adobe%20Photoshop.dmg"
-#  appcast 'https://github.com/sequelpro/sequelpro/releases.atom'
-  name 'Adobe Photoshop'
-  homepage 'https://www.rctest.com/'
-
-  depends_on macos: '>= :leopard'
-
-  app 'Adobe Photoshop.app'
-
+  homepage 'https://testtc.com'
+  version '201901'
+  sha256 '3cb3be0b2e5278d4eb27b3265a4a67044a2c1e11bc902fc15a6a237833dcc6e8'
+  link 'Adobe Photoshop.app'
 end
