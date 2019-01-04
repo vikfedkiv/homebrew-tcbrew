3740f5488d37414e90cf57c0add3a4edd3a23252
Viktor Fedkiv <v.fedkiv@tenantcloud.com>
2019-01-04 09:31:13 +0200

    change link to app

--- photoshop.rb
+++ photoshop.rb
@@ -3,5 +3,5 @@ class Photoshop < Formula
   sha256 "3cb3be0b2e5278d4eb27b3265a4a67044a2c1e11bc902fc15a6a237833dcc6e8"
   homepage 'https://testtc.com'
   version '201901'  
-  link 'Photoshop.app'
+  app 'Photoshop.app'
 end
